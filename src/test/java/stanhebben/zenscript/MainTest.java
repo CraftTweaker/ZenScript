cf24b0fabe4d7d1b31e8bd3c8ccbbc2357f3cd32
Jared <jaredlll08@gmail.com>
2018-02-19 22:22:29 +0200

    Added comments to the tests

--- src/test/java/stanhebben/zenscript/MainTest.java
+++ src/test/java/stanhebben/zenscript/MainTest.java
@@ -10,23 +10,31 @@ import static stanhebben.zenscript.ZenModule.compileScripts;
 
 public class MainTest {
     
-    public static final String fileName = "stringfile";
     
     public static void main(String[] args) throws IOException {
+        //Create a compile environment needed for the registry
         GenericCompileEnvironment compileEnvironment = new GenericCompileEnvironment();
+        //Creates a logger needed for the registry
         GenericErrorLogger logger = new GenericErrorLogger(System.out);
+        //Creates the IZenRegistry, needed to store all the ZenClass' and Symbols
         GenericRegistry registry = new GenericRegistry(compileEnvironment, logger);
+        //Registers A print function as a global method
         registry.registerGlobal("print", registry.getStaticFunction(GenericFunctions.class, "print", String.class));
+        //Creates a IEnvironmentGlobal needed to compile the scripts
         Map<String, byte[]> classes = new HashMap<>();
         IEnvironmentGlobal environmentGlobal = registry.makeGlobalEnvironment(classes);
-        
-        
-        ZenTokener parser = new ZenTokener(new FileReader("script.zs"), registry.getCompileEnvironment(), fileName, false);
+    
+        //Loads the script file
+        File file = new File("script.zs");
+        String fileName = file.getName();
+        FileReader reader = new FileReader(file);
+        //Creates a ZenTokener and ZenParsedFile for the file
+        ZenTokener parser = new ZenTokener(reader, registry.getCompileEnvironment(), fileName, false);
         ZenParsedFile zenParsedFile = new ZenParsedFile(fileName, fileName, parser, environmentGlobal);
         
         
         try {
-            // Stops if the compile is disabled
+            //Compiles and runs the script
             ZenModule.compileScripts(fileName, Collections.singletonList(zenParsedFile), environmentGlobal, false);
             
             ZenModule module = new ZenModule(classes, MainTest.class.getClassLoader());
