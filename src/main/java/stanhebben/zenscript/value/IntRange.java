56aca313d10725627e38b96e483ff4c61d29bb2a
kindlich <kindlich@gmx.net>
2019-10-20 19:13:22 +0200

    Added helper methods to IntRange to remove other helper class

--- src/main/java/stanhebben/zenscript/value/IntRange.java
+++ src/main/java/stanhebben/zenscript/value/IntRange.java
@@ -1,5 +1,7 @@
 package stanhebben.zenscript.value;
 
+import java.util.*;
+
 /**
  * @author Stan Hebben
  */
@@ -7,10 +9,12 @@ public class IntRange {
 
     private final int from;
     private final int to;
+    private final Random rand;
 
     public IntRange(int from, int to) {
         this.from = from;
         this.to = to;
+        rand = new Random(2906);
     }
 
     public int getFrom() {
@@ -19,5 +23,17 @@ public class IntRange {
 
     public int getTo() {
         return to;
+    }
+    
+    public int getMin() {
+        return getFrom();
+    }
+    
+    public int getMax() {
+        return getTo();
+    }
+    
+    public int getRandom() {
+        return rand.nextInt((to - from) + 1) + from;
     }
 }
